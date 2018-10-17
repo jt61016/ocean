bd0552db548c47a3e0a410272c420de6851fea73
jiangtao <>
2018-10-17 15:14:30 +0800

    Integer test
    Changes to be committed:
    	new file:   jdk-practices/src/main/java/com/jt61016/ocean/jdk/IntegerTest.java

---
+++ jdk-practices/src/main/java/com/jt61016/ocean/jdk/IntegerTest.java
@@ -0,0 +1,98 @@
+package com.jt61016.ocean.jdk;
+
+/**
+ * @author jiangtao
+ * @date 2018/9/13 下午3:27.
+ */
+public class IntegerTest {
+    final static char [] DigitTens = {
+            '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+            '1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '2', '2', '2', '2', '2', '2', '2', '2', '2', '2',
+            '3', '3', '3', '3', '3', '3', '3', '3', '3', '3',
+            '4', '4', '4', '4', '4', '4', '4', '4', '4', '4',
+            '5', '5', '5', '5', '5', '5', '5', '5', '5', '5',
+            '6', '6', '6', '6', '6', '6', '6', '6', '6', '6',
+            '7', '7', '7', '7', '7', '7', '7', '7', '7', '7',
+            '8', '8', '8', '8', '8', '8', '8', '8', '8', '8',
+            '9', '9', '9', '9', '9', '9', '9', '9', '9', '9',
+    } ;
+
+    final static char [] DigitOnes = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    } ;
+
+    /**
+     * All possible chars for representing a number as a String
+     */
+    final static char[] digits = {
+            '0' , '1' , '2' , '3' , '4' , '5' ,
+            '6' , '7' , '8' , '9' , 'a' , 'b' ,
+            'c' , 'd' , 'e' , 'f' , 'g' , 'h' ,
+            'i' , 'j' , 'k' , 'l' , 'm' , 'n' ,
+            'o' , 'p' , 'q' , 'r' , 's' , 't' ,
+            'u' , 'v' , 'w' , 'x' , 'y' , 'z'
+    };
+
+    /**
+     * 这段代码的详细分析:
+     * http://www.hollischuang.com/archives/1058
+     *
+     * @param i 需要转换的整数
+     * @param index 长度
+     * @param buf 输出
+     */
+    static void getChars(int i, int index, char[] buf) {
+        int q, r;
+        int charPos = index;
+        char sign = 0;
+
+        if (i < 0) {
+            sign = '-';
+            i = -i;
+        }
+
+        // Generate two digits per iteration
+        // 运算效率: 移位 > 乘法 > 除法
+        while (i >= 65536) {
+            q = i / 100;
+            // really: r = i - (q * 100);
+            r = i - ((q << 6) + (q << 5) + (q << 2));
+            i = q;
+            buf [--charPos] = DigitOnes[r];
+            buf [--charPos] = DigitTens[r];
+        }
+
+        // Fall thru to fast mode for smaller numbers
+        // assert(i <= 65536, i);
+        for (;;) {
+            // 约等于 i * 0.1,至于为什么选 52429 和 16+3,是因为考虑精确度以及运算过程中的溢出
+            // 右移运算中防止 (i * 52429)溢出 2^32, i需要小于2^16,所以上一个while循环判断条件是i >= 65536
+            q = (i * 52429) >>> (16+3);
+            r = i - ((q << 3) + (q << 1));
+            buf [--charPos] = digits [r];
+            i = q;
+            if (i == 0) break;
+        }
+        if (sign != 0) {
+            buf [--charPos] = sign;
+        }
+    }
+
+    public static void main(String[] args) {
+        int size = 7;
+        char[] buf = new char[size];
+		getChars(-68836321, size, buf);
+        String result = new String(buf);
+        System.out.println(result);
+    }
+}
